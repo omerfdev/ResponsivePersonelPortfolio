f11313731606b09a32692832a1b4ae4cf2128cf7
Ömer Almalı <78663695+omerfdev@users.noreply.github.com>
2023-08-01 20:28:37 +0300

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -24,11 +24,22 @@ window.onscroll = () => {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*='+ id +']').classList.add('active');
-            });            
+            });   
+            // active sections for animation on scroll   
+            sec.classList.add('show-animate');      
+        }
+        else{
+            sec.classList.remove('show-animate');      
+           
         }
     });
 
     // sticky header
     let header=document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY>100);
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+    
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
